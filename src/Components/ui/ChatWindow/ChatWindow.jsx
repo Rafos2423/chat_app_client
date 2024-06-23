55dc614e500ccc2830c0a8ff57dcdb1a5d7a2b0d
Rafos2423 <vladp2003kek@gmail.com>
2024-06-23 15:24:00 +0300

    - Добавлена автопрокрутка когда приходит новое сообщение;

--- src/Components/ui/ChatWindow/ChatWindow.jsx
+++ src/Components/ui/ChatWindow/ChatWindow.jsx
@@ -1,20 +1,25 @@
 import classes from './ChatWindow.module.css'
 import Message from "./Message/Message";
 import Input from "../Input/Input";
-import { useContext } from 'react';
+import { useContext, useRef, useEffect } from 'react';
 import { ConnectionContext } from '../../logic/Connection';
 
 const ChatWindow = () => {
     const { sendMessage, messageHistory, chatName } = useContext(ConnectionContext);
+    const messagesRef = useRef(null);
+
+    useEffect(() => {      
+        messagesRef.current?.lastElementChild?.scrollIntoView()
+    }, [messageHistory[chatName]]);
     
     return (
         <div className={classes.container}>
             <div className={classes.header}>{chatName}</div>
-            <div className={classes.messageHistory}>
+            <ul ref={messagesRef} className={classes.messageHistory}>
                 {messageHistory[chatName]?.map((x, i) => 
-                    <Message key={i} text={x.message} isMyMessage={x.isMyMessage} receiveAt={x.receiveAt}></Message>
+                    <li key={i}><Message text={x.message} isMyMessage={x.isMyMessage} receiveAt={x.receiveAt}></Message></li>
                 )}
-            </div>
+            </ul>
             {chatName && <div className={classes.inputContainer}>
                 <Input func={sendMessage} placeHolder={'Сообщение'} width={700} isDark={true}/>
             </div>}
